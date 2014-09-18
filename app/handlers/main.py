e0ffb6ea09bef377e57da6dfb657a2c92d5589d1
Axel Becerril <axel.becerril@grupojaque.com>
2014-09-18 18:21:36 -0500

    Mails are saved in database

--- app/handlers/main.py
+++ app/handlers/main.py
@@ -6,6 +6,11 @@ from app.model import email
 
 @route("(address)@(host)", address=".+")
 def START(message, address=None, host=None):
-    mail = email.Email(to_addr=address, body=dir(message))
+    mail = email.Email()
+    mail.to_addr = message['To']
+    mail.from_addr = message['From']
+    mail.subject = message['Subject']
+    mail.body = message.body()
+    mail.server = message.Peer
     mail.save()
 
--- app/model/email.py
+++ app/model/email.py
@@ -1,13 +1,15 @@
 from mongoengine import *
+import datetime
 
 class Email(Document):
     """
     How an email is stored in database.
     """
-    to_addr     = StringField()
-    from_addr   = StringField()
-    created_on  = DateTimeField()
+    to_addr     = StringField(db_field='to')
+    from_addr   = StringField(db_field='from')
+    created_on  = DateTimeField(default=datetime.datetime.now)
     subject     = StringField()
-    body        = ListField()
-    server      = StringField()
+    body        = StringField()
+    server      = ListField()
+    read        = BooleanField(default=False)
 
